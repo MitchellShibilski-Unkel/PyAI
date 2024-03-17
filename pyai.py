c387405f7803fc377f3bf20192eb8c211b7d1884
Mitchell Shibilski-Unkel <133730558+MitchellShibilski-Unkel@users.noreply.github.com>
2024-03-17 02:43:24 +0000

    PyAI

---
+++ pyai.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+def KNN(x, y, returnValues = 0):
+    distances = []
+    for axisX, axisY in zip(x, y):
+        distance = axisX - axisY
+        absDistance = np.absolute(distance)
+        distances.append(absDistance)
+        
+    sortedDistances = []
+    for d in distances:
+        checkDistance = min(distances, key = lambda x:np.absolute(x-i)) 
+        sortedDistances.append(checkDistance)
+        distances.remove(checkDistance)
+        
+    if returnValues == 0:
+        return sortedDistances[0]
+    else:
+        return sortedDistances[0:returnValues-1]
+    
+def RNN(w, u, b, x):
+    yt = 0
+    ht = 1 / 1 (w * x + u * yt ** -1 + b) ** -1
+    yt = 1 / 1 (w * ht + b) ** -1
+
+    return yt
+
+def linear_regression(x):
+    b = np.array(x).reshape(-1, 1)
+    y = X
